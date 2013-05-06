e3357b231fe0446fd4887205e6a738aeed6cfedb
Matt McKeon <matt@mmckeon.com>
2013-05-05 22:11:28 -0400

    Initial commit.
    With a fresh Rails 3.2 app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FoursquareCobotCheckin::Application.config.secret_token = '98b90ae451d9f3a15bbe476f8d20facd14e3e3b094e36e2f77e67e3c18b8d07e00ebed4083663e0672ae6c08e28a41d5e71a7a4da4c796b698e2ddcbf9374f31'
